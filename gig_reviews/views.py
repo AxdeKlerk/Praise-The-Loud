8982b76064ffd730f2f9d70e1c4759c4588dd90a
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-05-17 13:19:55 +0100

    Import Profile from models to view.py

--- gig_reviews/views.py
+++ gig_reviews/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from .forms import GigReviewForm
+from .forms import ProfileForm
 from django.utils.text import slugify
 from .models import Profile
-
 
 
 # Create your views here.
