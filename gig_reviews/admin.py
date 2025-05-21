232376b2662f666a29b61e2f8e25397d32dd26e7
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-05-21 12:12:49 +0100

    Add search functionality for artist, venue and fan to the admin panel in the admin.py file

--- gig_reviews/admin.py
+++ gig_reviews/admin.py
@@ -3,6 +3,16 @@ from .models import GigReview, Artist, Venue, Profile
 
 # Register your models here.
 admin.site.register(GigReview)
-admin.site.register(Artist)
-admin.site.register(Venue)
-admin.site.register(Profile)
+
+#Add search functionality to the admin panel
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    search_fields = ('user__username',)
