4543e33da5cdd08c156106d34d03ec0e40978484
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-06-05 15:46:57 +0100

    Remove env.py from version control

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ["SECRET_KEY"]=["=+7+za-s#q8$hbu4*dh+!fn3%og3rovmo#b0o2vob(mc=u56we"]
-
-os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_4G0KfQoeRAMd@ep-black-rice-a2lgcs8s.eu-central-1.aws.neon.tech/from_five_next_860639")
-
-os.environ["CLOUDINARY_CLOUD_NAME"] = "dqde3a83z"
-os.environ["CLOUDINARY_API_KEY"] = "445625969339627"
-os.environ["CLOUDINARY_API_SECRET"] = "X4JgGzIrzdWd6QoE26I9a0Zm6Vw"
