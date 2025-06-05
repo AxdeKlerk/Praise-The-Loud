e44a7d65f6e95951dd9fc2bb011667003be359d6
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-06-05 14:43:04 +0100

    Correct env.py from envy.py in gitignore Regenerate a new secret key and add it to env.py Remove old secret key from settings.py file

--- env.py
+++ env.py
@@ -1,8 +1,8 @@
 import os
 
-os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_4G0KfQoeRAMd@ep-black-rice-a2lgcs8s.eu-central-1.aws.neon.tech/from_five_next_860639")
+os.environ["SECRET_KEY"]=["=+7+za-s#q8$hbu4*dh+!fn3%og3rovmo#b0o2vob(mc=u56we"]
 
-os.environ.setdefault("SECRET_KEY", "<O@YejJzzW8)|Ij9c#$]M%J^`gAmX")
+os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_4G0KfQoeRAMd@ep-black-rice-a2lgcs8s.eu-central-1.aws.neon.tech/from_five_next_860639")
 
 os.environ["CLOUDINARY_CLOUD_NAME"] = "dqde3a83z"
 os.environ["CLOUDINARY_API_KEY"] = "445625969339627"
